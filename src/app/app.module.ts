665854a08d460570d8dce0e268908ba7ac66eb15
İBRAHİM ETHEM ÇALIŞKAN <ethem.caliskan@stu.bahcesehir.edu.tr>
2018-08-01 15:54:29 +0300

    'deneme'

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import {Camera} from '@ionic-native/camera';
+import {StreamingMedia} from '@ionic-native/streaming-media';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+//import { MediaCapture } from '@ionic-native/media-capture';
+//import { Media } from '@ionic-native/media';
+//import { File } from '@ionic-native/file';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { IonicStorageModule } from '@ionic/storage';
+//import { QRScanner } from '@ionic-native/qr-scanner';
+import { Geolocation } from '@ionic-native/geolocation';
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    StreamingMedia,
+    Camera,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    BarcodeScanner,
+    Geolocation
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,50 @@
+import { Component, ViewChild } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+//import { MediaCapture, MediaFile, CaptureError, CaptureVideoOptions,CaptureImageOptions } from '@ionic-native/media-capture';
+//import { Storage } from '@ionic/storage';
+//import { Media, MediaObject } from '@ionic-native/media';
+//import { File } from '@ionic-native/file';
+import { Geolocation } from '@ionic-native/geolocation';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+//import { QRScanner,QRScannerStatus } from '@ionic-native/qr-scanner';
+//const MEDIA_FILES_KEY = 'mediaFiles';
+ 
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+ // mediaFiles = [];
+  @ViewChild('myvideo') myVideo: any;
+  
+  constructor(private barcodeScanner:BarcodeScanner,public navCtrl: NavController,private geolocation:Geolocation) {}
+ public options:any;
+ public data:any;
+  scan() {
+  
+this.options = {
+        prompt : "Scan your barcode "
+    }
+    this.barcodeScanner.scan(this.options).then((barcodeData) => {
+        
+        console.log(barcodeData.text);
+        this.data = barcodeData;
+    }, (err) => {
+        console.log("Error occured : " + err);
+    });         
+
+  }
+
+  captureAudio(){
+this.geolocation.getCurrentPosition().then((resp) => {
+
+this.data="Okey";
+}).catch((error) => {
+  console.log('Error getting location'+error.message);
+  this.data=error.message;
+});
+  }
+ 
+ 
+}
